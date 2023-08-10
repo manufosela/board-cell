ee16d75b97bb31143729e3c5b80c9f638d5522c7
manufosela <manufosela@gmail.com>
2023-08-10 13:28:21 +0200

    feat: cell has color and content

--- src/BoardCell.js
+++ src/BoardCell.js
@@ -1,5 +1,5 @@
-import { LitElement, html } from "lit";
-import { BoardCellStyles } from "./board-cell-style.js";
+import { LitElement, html } from 'lit';
+import { BoardCellStyles } from './board-cell-style.js';
 
 /**
  * `board-cell`
@@ -12,7 +12,7 @@ import { BoardCellStyles } from "./board-cell-style.js";
 
 export class BoardCell extends LitElement {
   static get is() {
-    return "board-cell";
+    return 'board-cell';
   }
 
   static get properties() {
@@ -52,7 +52,7 @@ export class BoardCell extends LitElement {
        * @property
        * @type { Boolean }
        */
-       hoverCell: { type: Boolean, attribute: 'hover-cell' },
+      hoverCell: { type: Boolean, attribute: 'hover-cell' },
       /**
        * Hide Cell Lines
        * @property
@@ -64,7 +64,7 @@ export class BoardCell extends LitElement {
        * @property
        * @type { String }
        */
-       cellTextColor: { type: String, attribute: 'cell-text-color' },
+      cellTextColor: { type: String, attribute: 'cell-text-color' },
       /**
        * Grid color
        * @property
@@ -76,14 +76,14 @@ export class BoardCell extends LitElement {
        * @property
        * @type { String }
        */
-       backgroundColor: { type: String, attribute: 'cell-bg-color' },
+      backgroundColor: { type: String, attribute: 'cell-bg-color' },
       /**
        * Callback function to be called when the board is clicked
        * @property
        * @type { String }
        */
-      onclickCallback: {type: String, attribute: 'onclick' },
-    }
+      onclickCallback: { type: String, attribute: 'onclick' },
+    };
   }
 
   static get styles() {
@@ -101,6 +101,7 @@ export class BoardCell extends LitElement {
     this.hoverCell = false;
     this.mouseOverX = -1;
     this.mouseOverY = -1;
+    this.cellsBgColor = [];
     this.cellsContent = [];
     this.cellsWithoutEvent = [];
     this.cellTextColor = '#000000';
@@ -125,7 +126,7 @@ export class BoardCell extends LitElement {
   connectedCallback() {
     super.connectedCallback();
     this._initializeCells();
-    this.style.width = `${this.rows * this.cellSize  }px`;
+    this.style.width = `${this.rows * this.cellSize}px`;
     this._enableBoardEvents();
   }
 
@@ -140,23 +141,28 @@ export class BoardCell extends LitElement {
         id: this.id,
         componentName: this.tagName,
         component: this,
-      }
+      },
     });
     this.drawBoard();
     document.dispatchEvent(wcReadyEvent);
   }
 
   _enableBoardClick() {
-    if (this.onclickCallback && (this.parentElement[this.onclickCallback] || window[this.onclickCallback])) {
+    if (
+      this.onclickCallback &&
+      (this.parentElement[this.onclickCallback] || window[this.onclickCallback])
+    ) {
       this.addEventListener('click', this.boardClicked);
     }
   }
 
   _initializeCells() {
-    for(let i=0; i<this.rows; i+=1) {
+    for (let i = 0; i < this.rows; i += 1) {
+      this.cellsBgColor.push([]);
       this.cellsContent.push([]);
       this.cellsWithoutEvent.push([]);
-      for(let j=0; j<this.cols; j+=1) {
+      for (let j = 0; j < this.cols; j += 1) {
+        this.cellsBgColor[i].push(null);
         this.cellsContent[i].push(null);
         this.cellsWithoutEvent[i].push(false);
       }
@@ -164,47 +170,113 @@ export class BoardCell extends LitElement {
   }
 
   _enableBoardEvents() {
-    this._enableBoardClick();
     if (this.hoverCell) {
       this.addEventListener('mousemove', this.mouseOverCell);
       this.addEventListener('mouseout', this.mouseOutCell);
     }
-    document.addEventListener('board-cell__content-refresh', this.contentRefresh);
+    document.addEventListener(
+      'board-cell__content-refresh',
+      this.contentRefresh
+    );
     document.addEventListener('board-cell__change-cell-data', this.setCellData);
-    document.addEventListener('board-cell__change-all-cell-data', this.setCellsData);
-    document.addEventListener('board-cell__change-cell-content', this.changeCellContent);
-    document.addEventListener('board-cell__change-cells-content', this.changeCellsContent);
-    document.addEventListener('board-cell__clear-cell-content', this._clearCellContentCallback);
-    document.addEventListener('board-cell__clear-all-content', this._clearAllContentCallback);
-    document.addEventListener('board-cell__content-clear-row', this._clearRowContentCallback)
-    document.addEventListener('board-cell__content-clear-col', this._clearColContentCallback);
-    document.addEventListener('board-cell__enable-board-click', this._enableBoardClick);
-    document.addEventListener('board-cell__disable-board-click', this._disableBoardClick);
+    document.addEventListener(
+      'board-cell__change-all-cell-data',
+      this.setCellsData
+    );
+    document.addEventListener(
+      'board-cell__change-cell-content',
+      this.changeCellContent
+    );
+    document.addEventListener(
+      'board-cell__change-cells-content',
+      this.changeCellsContent
+    );
+    document.addEventListener(
+      'board-cell__clear-cell-content',
+      this._clearCellContentCallback
+    );
+    document.addEventListener(
+      'board-cell__clear-all-content',
+      this._clearAllContentCallback
+    );
+    document.addEventListener(
+      'board-cell__content-clear-row',
+      this._clearRowContentCallback
+    );
+    document.addEventListener(
+      'board-cell__content-clear-col',
+      this._clearColContentCallback
+    );
+    document.addEventListener(
+      'board-cell__enable-board-click',
+      this._enableBoardClick
+    );
+    document.addEventListener(
+      'board-cell__disable-board-click',
+      this._disableBoardClick
+    );
+    this._enableBoardClick();
   }
 
   _disableBoardClick() {
-    if (this.onclickCallback && (this.parentElement[this.onclickCallback] || window[this.onclickCallback])) {
+    if (
+      this.onclickCallback &&
+      (this.parentElement[this.onclickCallback] || window[this.onclickCallback])
+    ) {
       this.removeEventListener('click', this.boardClicked);
     }
   }
 
   _disableBoardEvents() {
-    this._disableBoardClick();
     if (this.hoverCell) {
       this.removeEventListener('mousemove', this.mouseOverCell);
       this.removeEventListener('mouseout', this.mouseOutCell);
     }
-    document.removeEventListener('board-cell__content-refresh', this.contentRefresh);
-    document.removeEventListener('board-cell__change-cell-data', this.setCellData);
-    document.removeEventListener('board-cell__change-all-cell-data', this.setCellsData);
-    document.removeEventListener('board-cell__change-cell-content', this.changeCellContent);
-    document.removeEventListener('board-cell__change-cells-content', this.changeCellsContent);
-    document.removeEventListener('board-cell__clear-cell-content', this._clearCellContentCallback);
-    document.removeEventListener('board-cell__clear-all-content', this._clearAllContentCallback);
-    document.removeEventListener('board-cell__content-clear-row', this._clearRowContentCallback)
-    document.removeEventListener('board-cell__content-clear-col', this._clearColContentCallback);
-    document.removeEventListener('board-cell__enable-board-click', this._enableBoardClick);
-    document.removeEventListener('board-cell__disable-board-click', this._disableBoardClick);
+    document.removeEventListener(
+      'board-cell__content-refresh',
+      this.contentRefresh
+    );
+    document.removeEventListener(
+      'board-cell__change-cell-data',
+      this.setCellData
+    );
+    document.removeEventListener(
+      'board-cell__change-all-cell-data',
+      this.setCellsData
+    );
+    document.removeEventListener(
+      'board-cell__change-cell-content',
+      this.changeCellContent
+    );
+    document.removeEventListener(
+      'board-cell__change-cells-content',
+      this.changeCellsContent
+    );
+    document.removeEventListener(
+      'board-cell__clear-cell-content',
+      this._clearCellContentCallback
+    );
+    document.removeEventListener(
+      'board-cell__clear-all-content',
+      this._clearAllContentCallback
+    );
+    document.removeEventListener(
+      'board-cell__content-clear-row',
+      this._clearRowContentCallback
+    );
+    document.removeEventListener(
+      'board-cell__content-clear-col',
+      this._clearColContentCallback
+    );
+    document.removeEventListener(
+      'board-cell__enable-board-click',
+      this._enableBoardClick
+    );
+    document.removeEventListener(
+      'board-cell__disable-board-click',
+      this._disableBoardClick
+    );
+    this._disableBoardClick();
   }
 
   contentRefresh(e) {
@@ -217,28 +289,45 @@ export class BoardCell extends LitElement {
     const rect = this.canvas.getBoundingClientRect();
     const x = Math.floor((ev.clientX - rect.left) / this.cellSize);
     const y = Math.floor((ev.clientY - rect.top) / this.cellSize);
-    if ((x <= this.cols-1 && y <= this.rows-1) && (x >= 0 && y >=0)) {
-      if ((x !== this.mouseOverX || y !== this.mouseOverY) && (this.mouseOverY >= 0 && this.mouseOverX >=0)) {
-        this._drawCellContent(this.mouseOverX * this.cellSize, this.mouseOverY * this.cellSize, this.cellsContent[this.mouseOverX][this.mouseOverY]);
+    if (x <= this.cols - 1 && y <= this.rows - 1 && x >= 0 && y >= 0) {
+      if (
+        (x !== this.mouseOverX || y !== this.mouseOverY) &&
+        this.mouseOverY >= 0 &&
+        this.mouseOverX >= 0
+      ) {
+        this._drawCellContent(
+          this.mouseOverX * this.cellSize,
+          this.mouseOverY * this.cellSize,
+          this.cellsContent[this.mouseOverX][this.mouseOverY]
+        );
       }
-      this.ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
-      this.ctx.fillRect(this.mouseOverX * this.cellSize, this.mouseOverY * this.cellSize, this.cellSize-1, this.cellSize-1);
+      this.ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
+      this.ctx.fillRect(
+        this.mouseOverX * this.cellSize,
+        this.mouseOverY * this.cellSize,
+        this.cellSize - 1,
+        this.cellSize - 1
+      );
       this.mouseOverX = x;
       this.mouseOverY = y;
     }
   }
 
   mouseOutCell() {
-    this._drawCellContent(this.mouseOverX * this.cellSize, this.mouseOverY * this.cellSize, this.cellsContent[this.mouseOverX][this.mouseOverY]);
- }
+    this._drawCellContent(
+      this.mouseOverX * this.cellSize,
+      this.mouseOverY * this.cellSize,
+      this.cellsContent[this.mouseOverX][this.mouseOverY]
+    );
+  }
 
   drawBoard() {
     const width = this.cols * this.cellSize;
     const height = this.rows * this.cellSize;
     this.canvas = this.shadowRoot.querySelector('canvas');
     this.ctx = this.canvas.getContext('2d');
-    for (let w = 0; w <= width; w+=this.cellSize) {
-      for (let h = 0; h <= height; h+=this.cellSize) {
+    for (let w = 0; w <= width; w += this.cellSize) {
+      for (let h = 0; h <= height; h += this.cellSize) {
         if (!this.hideCellLines) {
           this.ctx.moveTo(w, 0);
           this.ctx.lineTo(w, height);
@@ -254,11 +343,11 @@ export class BoardCell extends LitElement {
   }
 
   drawCellsContent() {
-    for(let row=0; row<this.rows; row+=1) {
-      for(let col=0; col<this.cols; col+=1) {
+    for (let row = 0; row < this.rows; row += 1) {
+      for (let col = 0; col < this.cols; col += 1) {
         const dx = col * this.cellSize;
         const dy = row * this.cellSize;
-        if(this.cellsContent[col][row]) {
+        if (this.cellsContent[col][row]) {
           this._drawCellContent(dx, dy, this.cellsContent[col][row]);
         } else {
           this.ctx.fillStyle = this.gridColor;
@@ -268,22 +357,28 @@ export class BoardCell extends LitElement {
     }
   }
 
-  _drawCellContent(dx, dy, content) {
-    if (content.toLowerCase().match(/^#[0-9a-f]{6}$/i)) {
-      this.ctx.fillStyle = content;
-      this.ctx.fillRect(dx, dy, this.cellSize, this.cellSize);
-    } else if (content.match(/^(http|https):\/\/[^ "]+$/i) || content.match(/(png|jpg|jpeg|gif|svg)$/i)) {
+  _drawCellContent(dx, dy, content, color) {
+    this.ctx.fillStyle = color;
+    this.ctx.fillRect(dx, dy, this.cellSize, this.cellSize);
+
+    if (
+      content.match(/^(http|https):\/\/[^ "]+$/i) ||
+      content.match(/(png|jpg|jpeg|gif|svg)$/i)
+    ) {
       const img = new Image();
       img.src = content;
       img.onload = () => {
-        this.ctx.drawImage(img, dx, dy, this.cellSize-1, this.cellSize-1);
-      }
+        this.ctx.drawImage(img, dx, dy, this.cellSize - 1, this.cellSize - 1);
+      };
     } else {
-      this.ctx.font = `bold ${this.cellSize/2}px Arial`;
+      this.ctx.font = `bold ${this.cellSize / 2}px Arial`;
       this.ctx.fillStyle = this.cellTextColor;
-      this.ctx.fillText(content, dx + this.cellSize/2 - 8, dy + this.cellSize/2 + 8);
+      this.ctx.fillText(
+        content,
+        dx + this.cellSize / 2 - 8,
+        dy + this.cellSize / 2 + 8
+      );
     }
-    
   }
 
   drawCell(x, y) {
@@ -293,11 +388,11 @@ export class BoardCell extends LitElement {
   }
 
   clearEventCell(x, y) {
-    this.cellsWithoutEvent[x-1][y-1] = true;
+    this.cellsWithoutEvent[x - 1][y - 1] = true;
   }
 
   setEventCell(x, y) {
-    this.cellsWithoutEvent[x-1][y-1] = false;
+    this.cellsWithoutEvent[x - 1][y - 1] = false;
   }
 
   setCellContent(x, y, content) {
@@ -307,7 +402,7 @@ export class BoardCell extends LitElement {
   setCellData(e) {
     if (e.detail.id === this.id) {
       this.setCellContent(e.detail.x, e.detail.y, e.detail.content);
-   }
+    }
   }
 
   setCellsContent(cells) {
@@ -322,21 +417,30 @@ export class BoardCell extends LitElement {
 
   changeCellContent(ev) {
     if (ev.detail.id === this.id) {
-      const {detail} = ev;
-      const {content} = detail;
+      const { detail } = ev;
+      const { content, color } = detail;
       const dx = detail.cellx * this.cellSize;
       const dy = detail.celly * this.cellSize;
+      this.cellsBgColor[detail.cellx][detail.celly] = color;
       this.cellsContent[detail.cellx][detail.celly] = content;
-      this._drawCellContent(dx, dy, this.cellsContent[detail.cellx][detail.celly]);
+      this._drawCellContent(
+        dx,
+        dy,
+        this.cellsContent[detail.cellx][detail.celly],
+        this.cellsBgColor[detail.cellx][detail.celly]
+      );
+      this.boardClicked(ev);
     }
   }
 
   changeCellsContent(ev) {
     if (ev.detail.id === this.id) {
-      const {detail} = ev;
-      const {content} = detail;
+      const { detail } = ev;
+      const { content, color } = detail;
       this.cellsContent = [...content];
+      this.cellsBgColor = [...color];
       this.drawCellsContent();
+      this.boardClicked(ev);
     }
   }
 
@@ -345,20 +449,21 @@ export class BoardCell extends LitElement {
       const rect = this.canvas.getBoundingClientRect();
       const cellx = Math.floor((ev.clientX - rect.left) / this.cellSize);
       const celly = Math.floor((ev.clientY - rect.top) / this.cellSize);
-      const newEvDetail = { detail:
-        {
+      const newEvDetail = {
+        detail: {
           mousex: ev.clientX - rect.left,
           mousey: ev.clientY - rect.top,
           cellx,
           celly,
-          content: this.cellsContent[cellx][celly]
-        }
-      };    
+          color: this.cellsBgColor[cellx][celly],
+          content: this.cellsContent[cellx][celly],
+        },
+      };
 
       if (!this.cellsWithoutEvent[cellx][celly]) {
         if (this.parentElement[this.onclickCallback]) {
           this.parentElement[this.onclickCallback](newEvDetail);
-        } else if(window[this.onclickCallback]) {
+        } else if (window[this.onclickCallback]) {
           window[this.onclickCallback](newEvDetail);
         }
       }
@@ -366,9 +471,10 @@ export class BoardCell extends LitElement {
   }
 
   clearColContent(colToClear) {
-    for(let i=0; i<this.rows; i+=1) {
-      for(let j=0; j<this.cols; j+=1) {
-        if(j === colToClear) {
+    for (let i = 0; i < this.rows; i += 1) {
+      for (let j = 0; j < this.cols; j += 1) {
+        if (j === colToClear) {
+          this.cellsBgColor[i][j] = null;
           this.cellsContent[i][j] = null;
         }
       }
@@ -382,9 +488,10 @@ export class BoardCell extends LitElement {
   }
 
   clearRowContent(rowToClear) {
-    for(let i=0; i<this.rows; i+=1) {
-      for(let j=0; j<this.cols; j+=1) {
-        if(i === rowToClear) {
+    for (let i = 0; i < this.rows; i += 1) {
+      for (let j = 0; j < this.cols; j += 1) {
+        if (i === rowToClear) {
+          this.cellsBgColor[i][j] = null;
           this.cellsContent[i][j] = null;
         }
       }
@@ -398,18 +505,20 @@ export class BoardCell extends LitElement {
   }
 
   clearCellContent(x, y) {
-    this.cellsContent[x-1][y-1] = null;
+    this.cellBgColor[x - 1][y - 1] = null;
+    this.cellsContent[x - 1][y - 1] = null;
   }
 
   _clearCellContentCallback(ev) {
-    if (ev.detail.id === this.id) {  
+    if (ev.detail.id === this.id) {
       this.clearCellContent(ev.detail.cellx, ev.detail.celly);
     }
   }
 
   clearAllContent() {
-    for(let i=0; i<this.rows; i+=1) {
-      for(let j=0; j<this.cols; j+=1) {
+    for (let i = 0; i < this.rows; i += 1) {
+      for (let j = 0; j < this.cols; j += 1) {
+        this.cellsBgColor[i][j] = null;
         this.cellsContent[i][j] = null;
       }
     }
@@ -424,8 +533,15 @@ export class BoardCell extends LitElement {
   render() {
     return html`
       <h1>${this.title}</h1>
-      <canvas id="${this.id}_canvas" width="${this.cols * this.cellSize}" height="${this.rows * this.cellSize}" class="board" role="img" aria-label="canvas with board" alt="Mineswipper board with ${this.cols}x${this.rows}"></canvas>
+      <canvas
+        id="${this.id}_canvas"
+        width="${this.cols * this.cellSize}"
+        height="${this.rows * this.cellSize}"
+        class="board"
+        role="img"
+        aria-label="canvas with board"
+        alt="Mineswipper board with ${this.cols}x${this.rows}"
+      ></canvas>
     `;
   }
-
 }
